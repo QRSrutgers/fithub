c8ba4b78289c4d954abd72eb8ad7839f597ceafb
kelly quinn <kellyquinn@kellys-MBP-2.fios-router.home>
2017-10-13 03:17:29 -0400

    social.js

--- assets/java/social.js
+++ assets/java/social.js
@@ -13,6 +13,7 @@ $(".event-selection").on("click", function() {
     linkForEvent = [];
     timeEvent = [];
     timeEventNew = [];
+    // displayTime = [];
 
     chosen = $(this).attr("value");
 
@@ -27,13 +28,28 @@ $(".event-selection").on("click", function() {
         .done(function(response) {
             console.log(response);
             for (var i = 0; i < 5; i++) {
-                timeEvent.push(response[i].time);
-                nameOfEvent.push(response[i].name);
-                linkForEvent.push(response[i].link);
-                timeEventNew.push(moment(timeEvent[i], "X"));
-                convertedTime.push((moment(timeEventNew[i]).format("H:HH, MM/DD")));
 
-                $("#event").append("<div>Meetup Event: " + nameOfEvent[i] + "</div><div>Start Time (24hrs): " + convertedTime[i] + "</div><div>Link for event page: <a href='" + linkForEvent[i] + "' target='_blank'>I'll take you there</a></div><br>");
+                nameOfEvent.push(response[i].name);
+
+                linkForEvent.push(response[i].link);
+                    console.log("link" + linkForEvent);
+
+                var day = moment.unix(response[i].time/1000).format("MM/DD/YYYY");
+                console.log("Day:" + day);
+
+                timeEvent.push(moment.unix(response[i].time/1000));
+                    console.log("Time Event:" + timeEvent);
+
+                timeEventNew.push(moment(timeEvent[i], "X"));
+                    console.log("TimeEventNew:" + timeEventNew);
+
+                convertedTime.push(moment(timeEventNew[i]).format("MM/DD/YYYY, HH:MM"));
+                    console.log("Converted time:" + convertedTime);
+
+                 // displayTime.push(convertedTime);
+                 //    console.log("Displayed Time:" + displayTime);   
+
+                $("#event").append("<div>Meetup Event: " + nameOfEvent[i] + "</div><div>Date & Time (24hrs): " + convertedTime[i] + "</div><div>Link for event page: <a href='" + linkForEvent[i] + "' target='_blank'>I'll take you there</a></div><br>");
 
             }
 
